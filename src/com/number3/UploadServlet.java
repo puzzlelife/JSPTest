d2ae88c6ee6229f5434198cb10d051f6951ebff0
puzzlielife <302703970@qq.com>
2017-03-14 15:34:16 +0800

    111

--- src/com/number3/UploadServlet.java
+++ src/com/number3/UploadServlet.java
@@ -1,6 +1,7 @@
 package com.number3;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,11 +14,26 @@ public class UploadServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse rep) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        req.setCharacterEncoding("UTF-8");
-        String name=req.getParameter("name");
-        String age=req.getParameter("age");
-        String file=req.getParameter("file");
-        rep.setContentType("text/html;charset=utf-8");
-        rep.getWriter().write("name:"+name+"<br>age:"+age+"<br>file:"+file);
+//        req.setCharacterEncoding("UTF-8");
+//        String name=req.getParameter("name");
+//        String age=req.getParameter("age");
+//        String file=req.getParameter("file");
+//        rep.setContentType("text/html;charset=utf-8");
+//        rep.getWriter().write("name:"+name+"<br>age:"+age+"<br>file:"+file);
+        process(req,rep);
     }
+
+    private void process(HttpServletRequest req, HttpServletResponse rep) throws IOException {
+        rep.setContentType("text/html;charset=UTF-8");
+        ServletInputStream sis=req.getInputStream();
+        byte[] buf=new byte[1024];
+        int flag=0;
+        while((flag=sis.readLine(buf,0,1024))!=-1){
+            String s=new String(buf,0,flag);
+            rep.getWriter().write(s+"<br/>");
+        }
+        sis.close();
+    }
+
+
 }
